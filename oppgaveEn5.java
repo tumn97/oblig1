8ade2d32526a5c02a5ebe0dd261b0dc055f3e65a
tumn97 <s325862@oslomet.no>
2018-09-03 13:51:41 +0200

    Begynnelsen på oppgave 1, har ikke testet

---
+++ oppgaveEn5.java
@@ -0,0 +1,20 @@
+package Algoritme.oblig;
+
+public class oppgaveEn5 {
+
+    public static void rotasjon(char[] a){
+        int indeks = 0;
+        int n = a.length;
+        for(int i = 0; i < a.length-1; i++){
+
+            char flytt = a[n];
+            a[n] = a[indeks];
+            a[indeks]  = flytt;
+            indeks++;
+
+
+        }
+
+    }
+
+}
