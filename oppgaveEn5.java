f481cdfb5784822d062108fdb71b2b8880d4b0c5
tumn97 <s325862@oslomet.no>
2018-09-03 19:06:16 +0200

    Oppgave 5 er ferdig. Fjern main. Var bare for testing

--- oppgaveEn5.java
+++ oppgaveEn5.java
@@ -1,20 +1,36 @@
 package Algoritme.oblig;
 
+import java.util.Arrays;
+
 public class oppgaveEn5 {
 
-    public static void rotasjon(char[] a){
-        int indeks = 0;
-        int n = a.length;
-        for(int i = 0; i < a.length-1; i++){
+    public static void main(String[] args){
+        char[] array = {'a','b','c','d','e','f','g','h','i','j'};
 
+
+            rotasjon(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+    public static void rotasjon(char[] a){
+        //indeks
+        int indeks = 0;
+        //lengden på arrayet
+        int n = a.length-1;
+        //itererer så lang arrayen a er.
+        for(int i = 0; i < a.length-1; i++){
+            //flytter på verdiene i arrayet
             char flytt = a[n];
             a[n] = a[indeks];
             a[indeks]  = flytt;
+            //øker indeks med 1.
+            //sånn at den første plassen ikke
+            //blir forstyrret.
             indeks++;
     //hjk
 
         }
-
+            System.out.println("test");
     }
 
 }
