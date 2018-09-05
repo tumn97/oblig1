f0915b036ad7709ca094d5e8079c89dc51411478
TheTreeThatGrows <s315611@stud.hioa.no>
2018-09-05 16:06:21 +0200

    Oppgave 9 - Final Patch

---
+++ Oppgave9.java
@@ -0,0 +1,43 @@
+public class Oppgave9 {
+
+    public static int[] tredjeMin(int[] a) {
+
+        int n = a.length;
+
+        if (n < 3) {
+            throw new IllegalArgumentException("a.length(" + n + ") < 3!");
+        }
+
+        int min1 = a[0];
+        int min2 = a[0];
+        int min3 = a[0];
+
+        int m1 = 0;
+        int m2 = 0;
+        int m3 = 0;
+
+        for (int i = 0; i < a.length; i++) {
+
+            if (min1 > a[i]) {
+
+                min2 = min1;
+                min1 = a[i];
+                m1 = i;
+
+            } else if (min2 > a[i]) {
+
+                min2 = a[i];
+                m2 = i;
+
+            } else if (min3 > a[i] && min2 < a[i]) {
+
+                min3 = a[i];
+                m3 = i;
+            }
+        }
+
+        int SmallNumberIndex[] = {m1, m2, m3};
+
+        return SmallNumberIndex;
+    }
+}
