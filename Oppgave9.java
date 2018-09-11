8c3ad912dc8fac800c788dfba6c38a2d319f7bdf
tumn97 <s325862@oslomet.no>
2018-09-11 17:08:53 +0200

    Oppgave 9, hvis noen vil prøve å løse den. har skrevet litt

--- Oppgave9.java
+++ Oppgave9.java
@@ -1,84 +1,80 @@
-    // Oppgave 6
+package Algoritme.oblig1;
 
-    public static void rotasjon(char[] a, int k) {
+import Algoritme.oblig.Oblig1;
 
-        int indeks = 0;
+import java.util.NoSuchElementException;
 
-        int n = a.length-1;
-
-        if(k == 1) {
-
-            for (int i = 0; i < n; i++) {
-
-                char flytt = a[n];
-                a[n] = a[indeks];
-                a[indeks] = flytt;
-                indeks++;
-            }
-
-        } else if (k < 0) {
-
-            for (int i = a.length; i > 1; i--) {
-
-                char flytt = a[n];
-                a[n] = a[indeks];
-                a[indeks] = flytt;
-                indeks++;
-            }
-
-        } else if (k > a.length) {
-
-            for (int i = 0; i < n; i++) {
-
-                char flytt = a[n];
-                a[n] = a[indeks];
-                a[indeks] = flytt;
-                indeks++;
-            }
-        }
-    }
-
-
-    // Oppgave 9
-    public static int[] tredjeMin(int[] a) {
-
+public class Oppgave9 {
+    public static int[] tredjeMin(int[] a){
+        //initialiserer n som tabellens lengde
+        Oblig1.indekssortering(a);
         int n = a.length;
-
-        if (n < 3) {
-            throw new NoSuchElementException("a.length(" + n + ") < 3!");
+        if(n < 3){
+            throw new NoSuchElementException("Tabellen inneholder mindre enn 3 verdier");
         }
 
-        int min1 = Integer.MAX_VALUE;
-        int min2 = Integer.MAX_VALUE;
-        int min3 = Integer.MAX_VALUE;
-
+        //initialiserer posisjonene til minste, nest minste og tredje minste verdi
         int m1 = 0;
-        int m2 = 0;
-        int m3 = 0;
-
-        for (int i = 0; i < a.length; i++) {
-
-            if (a[i] < min1) {
-
-                min3 = min2;
-                min2 = min1;
-                min1 = a[i];
-                m1 = i;
-
-            } else if (a[i] < min2) {
-
-                min3 = min2;
-                min2 = a[i];
-                m2 = i;
-
-            } else if (a[i] < min3) {
-
-                min3 = a[i];
-                m3 = i;
-            }
+        int m2= 1;
+        int m3 = 2;
+        if(a.length == 3){
+            return new int[] {m1,m2,m3};
         }
 
-        int[] index = {m1, m2, m3};
+//            System.out.println(a[m1]);
+//            System.out.println(a[m2]);
+//            System.out.println(a[m3]);
 
-        return index;
+
+
+        //hvis den nest minste er større enn den minste. bytt
+        if(a[1] < a[0]){
+            m2 = 0;
+            m1 = 1;
+        }
+        //hvis den tredj minste er mindre enn a[0] bytt
+        if(a[2] < a[0]){
+            m3 = 0;
+            m1 = 2;
+        }
+        if(a[1] < a[2]){
+            m1= 2;
+            m3= 1;
+        }
+        //minste verdi
+        int minstverdi = a[m1];
+        int nestminst = a[m2];
+        int nestNestminst= a[m3];
+
+        for(int i = 3; i < n; i++){
+
+            if(a[i] < nestNestminst){
+
+                m3 = i;
+                nestNestminst = a[i];
+
+                if(a[i] <= nestminst){
+
+                    m3= m2;
+                    nestNestminst = nestminst;
+
+                    m2 = i;
+                    nestminst = a[i];
+                    if(a[i] < minstverdi){
+                        m2 = m1;
+                        nestminst = minstverdi;
+
+                        m1 = i;
+                        minstverdi = a[i];
+
+                    }
+                }else{
+                    m2 = i;
+                    nestminst=a[i];
+                }
+            }
+
+        }
+        return new int[] {m1,m2,m3};
     }
+}
