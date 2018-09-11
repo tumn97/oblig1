4671dea419fb18196ec64d12044d654fef0be472
Daniel Nguyen <dani12@hotmail.no>
2018-09-11 23:12:35 +0200

    Oppgave 10. Ikke ferdig

--- Oppgave10.java
+++ Oppgave10.java
@@ -1,37 +1,27 @@
+import java.util.Arrays;
+
 public class Oppgave10 {
 
 
-    /*
-    Metoden fungerer ikke som den skal fordi
-    boolean match utgir true eller false kun ut i fra
-    siste sammenligning. Altså de sammenligningene
-    som blir gjort tidligere spiller ingen rolle.
-     */
-
     public static boolean inneholdt(String a, String b) {
 
-        char m;
-        char n;
-        boolean match = true;
-        //ArrayList<Boolean> matching = new ArrayList<>();
+        char[] A = a.toCharArray();
+        char[] B = b.toCharArray();
+        Arrays.sort(A);
+        Arrays.sort(B);
 
-        for (int i = 0; i < a.length(); i++) {
-            m = a.charAt(i);
-            for (int j = 0; j < b.length(); j++) {
-                n = b.charAt(j);
-                if(m != n) {
-                    match = false;
-                    //matching.add(false);
-                    System.out.println(m + " " + n);
-                } else {
-                    match = true;
-                    //matching.add(true);
-                    System.out.println(m + " " + n);
-                }
-            }
+        int i = 0;
+        int j = 0;
+
+        while (i < a.length() && j < b.length()) {
+            if (A[i] < B[j])
+                i++;
+            else if (A[i] == B[j]) {
+                i++;
+                j++;
+            } else return false;
         }
+        return true;
 
-
-        return match;
     }
 }
