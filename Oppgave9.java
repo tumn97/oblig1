c20e61ccc46e96a9a4f2803803520eb7955a8b51
TheTreeThatGrows <s315611@stud.hioa.no>
2018-09-13 16:07:16 +0200

    Oppgave 9 Finished

--- Oppgave9.java
+++ Oppgave9.java
@@ -4,73 +4,74 @@ import Algoritme.oblig.Oblig1;
 
 import java.util.NoSuchElementException;
 
-    // Oppgave 9 (unfinished)
+   // Oppgave 9
     public static int[] tredjeMin(int[] a) {
 
         int n = a.length;
-        if (n < 3){
+        if(n < 3){
             throw new NoSuchElementException("a.length(" + n + ") < 3!");
         }
 
-        int m1 = 0;
-        int m2 = 1;
-        int m3 = 2;
+        if (n <= 3) {
+            int[] indeks1 = new int[3];
+            int[] temp1 = new int[n];
 
-        if(a.length == 3){
-            return new int[] {m1, m2, m3};
-        }
-
-        if(a[1] < a[0]){
-            m2 = 0;
-            m1 = 1;
-        }
-
-        if(a[2] < a[0]){
-            m3 = 0;
-            m1 = 2;
-        }
-
-        if(a[1] < a[2]){
-            m1 = 2;
-            m3 = 1;
-        }
-
-        int min1 = a[m1];
-        int min2 = a[m2];
-        int min3= a[m3];
-
-        for (int i = 3; i < n; i++){
-
-            if (a[i] < min3){
-
-                m3 = i;
-                min3 = a[i];
-
-                if (a[i] <= min2){
-
-                    m3= m2;
-                    min3 = min2;
-
-                    m2 = i;
-                    min2 = a[i];
-
-                    if (a[i] < min1){
-                        m2 = m1;
-                        min2 = min1;
-
-                        m1 = i;
-                        min1 = a[i];
-
-                    }
-                } else {
-
-                    m2 = i;
-                    min2=a[i];
-                }
+            //if (a.length == 0)return indeks;
+            for (int i = 0; i < n; i++) {
+                temp1[i] = a[i];
             }
 
-        }
+            boblesortering(temp1);
 
-        int indeks[] = {m1, m2, m3};
-        return indeks;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (temp1[i] == a[j]) {
+                        indeks1[i] = j;
+                    }
+                }
+            }
+            return indeks1;
+
+        } else if (n > 3 && n <= 6) {
+
+            int[] indeks2 = new int[6];
+            int[] temp2 = new int[6];
+
+            //if (a.length == 0)return indeks;
+            for (int i = 0; i < 6; i++) {
+                temp2[i] = a[i];
+            }
+
+            boblesortering(temp2);
+
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (temp2[i] == a[j]) {
+                        indeks2[i] = j;
+                    }
+                }
+            }
+            return indeks2;
+
+        } else {
+
+            int[] indeks3 = new int[10];
+            int[] temp3 = new int[10];
+
+            //if (a.length == 0)return indeks;
+            for (int i = 0; i < 10; i++) {
+                temp3[i] = a[i];
+            }
+
+            boblesortering(temp3);
+
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (temp3[i] == a[j]) {
+                        indeks3[i] = j;
+                    }
+                }
+            }
+            return indeks3;
+        }
     }
